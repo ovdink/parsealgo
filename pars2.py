5a81a2110b474c18477e8fe486de8f9182865eca
ovdink <ovdink@gmail.com>
2019-07-01 16:42:32 +0300

    update script

--- pars2.py
+++ pars2.py
@@ -9,48 +9,15 @@ db = create_engine('sqlite:///app.db')
 conn = db.connect()
 allFiles = os.listdir(path)
 
-#df_new = pd.DataFrame(np.nan, index=range(200), columns=['Наименование сервиса', 'Описание сервиса', 'Владелец', 'Блок', 'Индекс', 'Подразделение - владелец сервиса', 'Принадлежность к группе', 'Количество пользователей', 'Группировка для веб-формы опроса', 'КПЭ "Удовлетворенность внутренних клиентов" в проект/программу включен'])
-#df_leaders = pd.DataFrame(np.nan, index = range(300), columns=['Должность', 'ФИО', 'Вес КПЭ в целях (если применимо)', 'Индекс', 'ГВК'])
-#df_leaders = df_leaders.fillna(0)
-#df_preleaders = pd.DataFrame(np.nan, index = range(300), columns=['Должность', 'ФИО', 'Вес КПЭ в целях (если применимо)', 'Индекс', 'ГВК'])
-#df_preleaders = df_leaders.fillna(0)
-#df_projects = pd.DataFrame(np.nan, index=range(200), columns=['Индекс', 'ID', 'Название проекта/программы'])
-#dict_new = {'Наименование сервиса',
-#             'Описание сервиса',
-#             'Владелец',
-#             'Блок',
-#             'Индекс',
-#             'Подразделение - владелец сервиса',
-#             'Принадлежность к группе',
-#             'Количество пользователей',
-#             'Группировка для веб-формы опроса',
-#             'КПЭ "Удовлетворенность внутренних клиентов" в проект/программу включен'}
 df_new = pd.DataFrame()
 df_project = pd.DataFrame()
 df_leaders = pd.DataFrame()
 df_preleaders = pd.DataFrame()
 
-
 dict_new = {}
 dict_projects = {}
 dict_leaders = {}
 dict_preleaders = {}
-
-#df_new = pd.DataFrame(columns=['Наименование сервиса', 'Описание сервиса', 'Владелец', 'Блок', 'Индекс', 'Подразделение - владелец сервиса', 'Принадлежность к группе', 'Количество пользователей', 'Группировка для веб-формы опроса', 'КПЭ "Удовлетворенность внутренних клиентов" в проект/программу включен'])
-#df_new = df_new.append([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-#print ('ДФ_НЬЮ\n', df_new)
-#df_leaders = pd.DataFrame(columns=['Должность', 'ФИО', 'Вес КПЭ в целях (если применимо)', 'Индекс', 'ГВК'])
-#df_leaders.append([0, 1, 2, 3, 4])
-#df_leaders = df_leaders.fillna(0)
-#df_preleaders = pd.DataFrame(columns=['Должность', 'ФИО', 'Вес КПЭ в целях (если применимо)', 'Индекс', 'ГВК'])
-#df_preleaders.append([0, 1, 2, 3, 4])
-#df_preleaders = df_leaders.fillna(0)
-#df_projects = pd.DataFrame(columns=['Индекс', 'ID', 'Название проекта/программы'])
-#df_projects.append([0, 1, 2])
-#dic = {}
-#cols = ['Наименование сервиса', 'Описание сервиса', 'Владелец', 'Блок', 'Индекс', 'Подразделение - владелец сервиса', 'Принадлежность к группе', 'Количество пользователей', 'Группировка для веб-формы опроса', 'КПЭ "Удовлетворенность внутренних клиентов" в проект/программу включен']
-#for i in range(12,0,-1):
-#    dic[str(i)] = df[cols].shift(i).add_sufix(str(i))
        
 n0 = n1 = n3 = n4 = n5 = n6 = n7 = n8 = 0
 l0 = l1 = l2 = l3 = l4 = 0
@@ -114,11 +81,12 @@ for file in allFiles:
                         l1 = row[3]
                         l3 = n4
                         l4 = 'ГВК-13'
-                        dict_leaders = [{'Должность' : l0,
-                                         'ФИО' : l1,
-                                         'Вес КПЭ в целях (если применимо)' : l2,
-                                         'Индекс' : l3,
-                                         'ГВК' : l4}]
+                        dict_leaders = [{'id3' : str(l3) + '_GVK-13_1',
+                                         'position' : l0,
+                                         'full_name' : l1,
+                                         'kpi_weight' : l2,
+                                         'index' : l3,
+                                         'GVK' : l4}]
                         dToFrame2 = pd.DataFrame.from_dict(dict_leaders)
                         df_leaders = df_leaders.append(dToFrame2)
                    
@@ -127,62 +95,50 @@ for file in allFiles:
                         pl1 = row[3]
                         pl3 = n4
                         pl4 = 'ГВК-13'
-                        dict_preleaders = [{'Должность' : pl0,
-                                            'ФИО' : pl1,
-                                            'Вес КПЭ в целях (если применимо)' : pl2,
-                                            'Индекс' : pl3,
-                                            'ГВК' : pl4}]
+#                                            'Должность' : pl0,
+#                                            'ФИО' : pl1,
+#                                            'Вес КПЭ в целях (если применимо)' : pl2,
+#                                            'Индекс' : pl3 + 'ГВК-13_1',
+#                                            'ГВК' 
+                        dict_preleaders = [{'id3' : str(pl3) + '_GVK-13_1',
+                                            'position' : pl0,
+                                            'full_name' : pl1,
+                                            'kpi_weight' : pl2,
+                                            'index' : pl3,
+                                            'GVK' : pl4}]
                         dToFrame3 = pd.DataFrame.from_dict(dict_preleaders)
                         df_preleaders = df_preleaders.append(dToFrame3)
-                    #if flag2 == 0 and len(df_preleaders)
-#                    elif flag2 == 1 and row[0] != 'Должность' and row[0] != 0:
-#                        print(1)
                         
-                df_leaders = df_leaders[df_leaders.ФИО != 0]       
-                dict_project = [{'Индекс' : pr0,
+                #df_leaders = df_leaders[df_leaders.full_name[-9:] != 0]       
+                dict_project = [{'id3' : str(pr0) + '_GVK-13_1',
+                                 'index' : pr0,
                                  'ID' : pr1,
-                                 'Название проекта/программы' : pr2}]
+                                 'project_name' : pr2}]
                 dToFrame0 = pd.DataFrame.from_dict(dict_project)
                 df_project = df_project.append(dToFrame0)
     
-                dict_new = [{'Наименование сервиса': n0,
-                             'Описание сервиса': n1,
-                             'Владелец' : n2,
-                             'Блок' : n3,
-                             'Индекс': n4,
-                             'Подразделение - владелец сервиса' : n5,
-                             'Принадлежность к группе' : n6,
-                             'Количество пользователей' : n7,
-                             'Группировка для веб-формы опроса' : n8}]
+#                             'Наименование сервиса': n0,
+#                             'Описание сервиса': n1,
+#                             'Владелец' : n2,
+#                             'Блок' : n3,
+#                             'Индекс': n4,
+#                             'Подразделение - владелец сервиса' : n5,
+#                             'Принадлежность к группе' : n6,
+#                             'Количество пользователей' : n7,
+#                             'Группировка для веб-формы опроса' : n8}]
+                dict_new = [{'id3' : str(n4) + '_GVK-13_1',
+                             'service_name': n0,
+                             'service_desc': n1,
+                             'owner' : n2,
+                             'block' : n3,
+                             'index': n4,
+                             'unit' : n5,
+                             'membership' : n6,
+                             'number_user' : n7,
+                             'web_form' : n8}]
                 dToFrame1 = pd.DataFrame.from_dict(dict_new)
                 df_new = df_new.append(dToFrame1)
                 
-                
-                  
-                    
-#                dict_leaders = [{'Должность' : l0,
-#                                 'ФИО' : l1,
-#                                 'Вес КПЭ в целях (если применимо)' : l2,
-#                                 'Индекс' : l3,
-#                                 'ГВК' : l4}]
-#                dToFrame2 = pd.DataFrame.from_dict(dict_leaders)
-#                df_leaders = df_leaders.append(dToFrame2)
-#                    if flag == 1 and row[0] != 'Должность' and row[0] != 0 and len(dict_leaders) == 0: #df_leaders.iloc[j, 0] == 0:
-#                        df_leaders.iloc[j, 0] = row[0]
-#                        df_leaders.iloc[j, 1] = row[3]
-#                        df_leaders.iloc[j, 3] = df_new.iloc[l, 4]
-#                        df_leaders.iloc[j, 4] = 'ГВК-13'
-#                        j += 1
-#                        print ('строки leaders', j)
-#                    if row[0] == 'Должность' and df_preleaders.iloc[k, 0] == 0 and flag == 0 and dolCount == 1:
-#                        flag2 = 1
-#                    if flag2 == 1 and row[0] != 'Должность' and row[0] != 0 and df_preleaders.iloc[k, 0] == 0:
-#                        df_preleaders.iloc[k, 0] = row[0]
-#                        df_preleaders.iloc[k, 1] = row[3]
-#                        df_preleaders.iloc[k, 3] = df_new.iloc[l, 4]
-#                        df_preleaders.iloc[k, 4] = 'ГВК-13'
-#                        k += 1
-#                        print ('строки preleaders', k)
                 print('Создали файл\n')
 
         df_leaders.to_sql('_leaders', con = db, if_exists='append')
